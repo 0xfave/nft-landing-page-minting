eaf9fd5588f8c45f40443a95db08237908de1936
0xfavouradetona <favouradetona7@gmail.com>
2022-08-07 12:32:48 -0400

    team updated

--- src/components/sections/Faq.js
+++ src/components/sections/Faq.js
@@ -1,4 +1,24 @@
 import React from 'react'
+import styled from 'styled-components';
+
+const Section = styled.section`
+  min-height: 100vh;
+  width: 100vw;
+  background-color: ${(props) => props.theme.text};
+  position: relative;
+`;
+
+const Title = styled.h1`
+  font-size: ${(props) => props.theme.fontxxl};
+  text-transform: capitalize;
+  color: ${(props) => props.theme.text};
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 1rem auto;
+  border-bottom: 2px solid ${(props) => props.theme.text};
+  width: fit-content;
+`;
 
 const Faq = () => {
   return (
--- src/components/sections/Home.js
+++ src/components/sections/Home.js
@@ -1,8 +1,8 @@
 import React from "react";
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 import CoverVideo from "../CoverVideo";
 import TypeWriterText from "../TypeWriterText";
-import RoundTextBlack from '../../assets/Rounded-Text-Black.png';
+import RoundTextBlack from "../../assets/Rounded-Text-Black.png";
 
 const Section = styled.section`
   min-height: ${(props) => `calc(100vh - ${props.theme.navHeight})`};
@@ -31,18 +31,45 @@ const Box = styled.div`
   align-items: center;
 `;
 
-const Round = styled.div`
-position: absolute;
-bottom: 2rem;
-right: 90%;
-width: 6rem;
-height: 6rem;
-
-img{
-  width: 100%;
-  height: auto;
+const rotate = keyframes`
+100%{
+  transform: rotate(1turn);
 }
-`
+`;
+
+const Round = styled.div`
+  position: absolute;
+  bottom: 2rem;
+  right: 90%;
+  width: 6rem;
+  height: 6rem;
+  border: 1px solid ${(props) => props.theme.text};
+  border-radius: 50%;
+
+  img {
+    width: 100%;
+    height: auto;
+    animation: ${rotate} 6s linear infinite reverse;
+  }
+`;
+
+const Circle = styled.span`
+  width: 3rem;
+  height: 3rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 50%;
+
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+
+  background-color: ${(props) => props.theme.text};
+  color: ${(props) => props.theme.body};
+  font-size: ${(props) => props.theme.fontxl};
+`;
 
 const Home = () => {
   return (
@@ -55,6 +82,7 @@ const Home = () => {
           <CoverVideo />
         </Box>
         <Round>
+          <Circle>&#x2193;</Circle>
           <img src={RoundTextBlack} alt="NFT" />
         </Round>
       </Container>
